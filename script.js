944589a27ffccfc3ce4e05e0cec7b0c9f63893ba
Jason <jasonxie3@gmail.com>
2021-05-31 16:01:39 -0700

    add basic UI and interactivity

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ let result;
 let computerScore = 0;
 let playerScore = 0;
 
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+rockButton.addEventListener('click', game);
+paperButton.addEventListener('click', game);
+scissorsButton.addEventListener('click', game);
 
 function computerPlay() {
     let randomNum = Math.floor(Math.random()*3);    // Random number between 0 and 2, inclusive
@@ -23,18 +30,6 @@ function computerPlay() {
     return computerSelection;
 }
 
-// Get user input: rock, paper, or scissors
-function getUserInput() {
-    playerSelection = prompt("Rock, Paper, or Scissors").toString().toLowerCase();
-    // playerSelection = playerSelection.toString().toLowerCase();
-    if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
-        return playerSelection;   
-    } else {
-        alert("Please enter rock, paper, or scissors!");
-        getUserInput();
-    }
-    return playerSelection;
-}
 // If user selects rock, wins if computer selected scissors, loses if paper, ties if rock
 // If user selects paper, wins if computer selected rock, loses if scissor, ties if paper
 // If user selects scissors, wins if computer selected paper, loses if rock, ties if scissors
@@ -62,25 +57,28 @@ function play(playerSelection, computerSelection) {
     }
     return result;
 }
+function updateText(text) {
+    const resultsText = document.querySelector('#results-text');
+    resultsText.textContent = `You ${text}! The score is You: ${playerScore} vs Computer: ${computerScore}`;
+}
 
 // Play five games, keeping track of score
 function game() {
-    for (let i = 0; i < 5; i++) {
         computerPlay();
-        console.log(computerSelection);
-        getUserInput();
+        // console.log(computerSelection);
+        playerSelection = this.value;
         play(playerSelection, computerSelection);
         switch (result) {
             case "Win":
-                alert(`Computer picked ${computerSelection}, you win!`);
                 ++playerScore;
+                updateText('Win');
                 break;
             case "Lose":
-                alert(`Computer picked ${computerSelection}, you lose!`);
                 ++computerScore;
+                updateText('Lose');
                 break;
             case "Tie":
-                alert(`You both picked ${computerSelection}!`);
+                updateText('Tied');
                 break; 
         }
 
@@ -92,11 +90,10 @@ function game() {
             alert("You lose the game!");
             return;
         }
+        //console.log(playerScore);
+        //console.log(computerScore);
         */
-        console.log(playerScore);
-        console.log(computerScore);
-    }
-    
+    /*
     if (playerScore > computerScore) {
         alert(`You win the game! Final score is You ${playerScore} : Computer ${computerScore}.`);
     } else if (playerScore < computerScore) {
@@ -104,5 +101,6 @@ function game() {
     } else {
         alert(`It's a tie! Final score is You ${playerScore} : Computer ${computerScore}.`);
     }
+    */
 }
 // Report winner at end.
